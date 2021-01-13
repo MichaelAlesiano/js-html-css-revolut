b4a22032ec3e3ecbcf102371b18e95ab5881b6f5
Michael <michael.alesiano@gmail.com>
2021-01-13 15:39:25 +0100

    First push

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $('.drop-menu').hover(function(){
+    $(this).children('.dropdown').slideToggle();
+
+    $(this).find('i').toggleClass('active');
+
+  });
+
+});
